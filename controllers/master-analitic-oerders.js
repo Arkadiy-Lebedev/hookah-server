ba77f0ae03318c7681397cdc8f28ebd79a82b782
ArkadiyLebedev <bepluvdt@gmail.com>
2023-08-20 15:19:54 +0300

    fix statistic

--- controllers/master-analitic-oerders.js
+++ controllers/master-analitic-oerders.js
@@ -4,42 +4,8 @@ const {connection} = require("./db");
 
 
 
-// const getOrderMaster = async (req, res) => {  
-//   const { id } = req.body 
-//   console.log(req.body)
-//     try {       
-//       // полчаем все заказы за дату
-//      const resp = await connection.query(     
-//           `SELECT booking.id 
-//        FROM booking WHERE  DATE(time_start) = ?`, [id]);   
-
-
-//        // формируем по заказам общий заказ
-//        let total = 0
-// const iterations = resp[0].length
-// let arr = []
-//        for (let i = 0; i < iterations; i++) {
-//         try {
-//           const resp2 = await connection.execute(`SELECT orders.id, products.name, booking.id AS booking, table_room.number, products.price, orders.count, DATE_FORMAT(booking.time_start,'%H:%i') AS "timeStart",  booking.status,  booking.sales 
-//           FROM products RIGHT JOIN orders ON products.id = orders.product_id LEFT JOIN booking ON orders.booking_id = booking.id LEFT JOIN table_room ON booking.table_id = table_room.id WHERE  booking.id = ?`, [resp[0][i].id]);
-        
-//           const summary = resp2[0].reduce((acc, num) => acc + (num.price * num.count) - (num.price * num.count * (num.sales/100)), 0) 
-//              total += summary  
-//           const summ = summary     
-          
-//           arr.push({key:resp2[0][0].id, label:resp2[0][0].number, booking:resp2[0][0].booking, time:resp2[0][0].timeStart, summ: summ, children:resp2[0]})
-//         } catch (error) {
-//           console.error('Error fetching data:', error);
-//     }
-//    }
-// //  console.log(arr)
-//          res.status(200).json({data: arr, total:total})   
-          
-//     } catch {
-//       res.status(500).json({ status: "error", message: "Не удалось получить список , повторите попытку познее" });
-//     }
-// };
-
+//группировка и сумма по месяцам
+// select Month(DATE(time_start)), SUM(total_price) FROM `booking` GROUP BY Month(DATE(time_start))
 
 
 const getOrderMaster = async (req, res) => {  
@@ -52,12 +18,19 @@ const getOrderMaster = async (req, res) => {
      LEFT JOIN table_room ON booking.table_id = table_room.id      
      WHERE  booking.id = ?`, [id]);
 
-     const resp2 = await connection.execute(`SELECT clients.name 
+
+      if (resp[0].length > 0) {
+        const resp2 = await connection.execute(`SELECT name, phone 
      FROM clients       
      WHERE  id = ?`, [resp[0][0].client_id]);
+     res.status(200).json({data: resp[0], client:resp2[0]}) 
+      } else {
+      res.status(200).json({data: resp[0], client:[]})   
+     }
+   
      
 
-         res.status(200).json({data: resp[0], client:resp2[0]})   
+         
           
     } catch {
       res.status(500).json({ status: "error", message: "Не удалось получить список , повторите попытку познее" });
